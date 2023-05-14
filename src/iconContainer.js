7b57cdbd2fb2b0766b14beffc73edc0f48bfb797
7070509785 <nutankumari7262@gmail.com>
2023-05-14 13:19:40 +0530

    iconContainer.module

--- src/iconContainer.js
+++ src/iconContainer.js
@@ -1,44 +1,56 @@
-import "./iconContainer.css";
+import styles from "./iconContainer.module.css";
 function IconContainer() {
   return (
-    <div className="himanshu">
-      <div className="nutan">
+    <div className={styles.himanshu}>
+      <div className={styles.nutan}>
         <h2>What customers say about this product</h2>
         <span>X</span>
       </div>
-      <div className="nutan-mandal">
-        <div className="kajal">
+      <div className={styles["nutan-mandal"]}>
+        <div className={styles.kajal}>
           <p>Fit</p>
-          <div className="bittu">
-            <progress id="file" value="4" max="100"></progress>
+          <div className={styles.bittu1}>
+            <progress value="4" max="100"></progress>
             <p>Tight(4%)</p>
           </div>
-          <div className="bittu1">
-            <progress id="file" value="4" max="100"></progress>
-            <p>Tight(4%)</p>
-          </div>
-          <div className="bittu2">
-            <progress id="file" value="4" max="100"></progress>
+          <div className={styles.bittu1}>
+            <progress value="4" max="100"></progress>
             <p>ALittle Tight(4%)</p>
           </div>
-          <div className="bittu3">
-            <progress id="file" value="70" max="100"></progress>
+          <div className={styles.bittu1}>
+            <progress value="70" max="100"></progress>
             <p>Just Right(73%)</p>
           </div>
-          <div className="bittu4">
-            <progress id="file" value="12" max="100"></progress>
+          <div className={styles.bittu1}>
+            <progress value="12" max="100"></progress>
             <p>ALittle Loose(12%)</p>
           </div>
-          <div className="bittu5">
-            <progress id="file" value="7" max="100"></progress>
+          <div className={styles.bittu1}>
+            <progress value="7" max="100"></progress>
             <p>Loose(7%)</p>
           </div>
         </div>
-        <div timtim>
+        <div className={styles["timtim-mandal"]}>
           <p>Lenght</p>
-          <div className="bittu">
-            <progress id="file" value="4" max="100"></progress>
-            <p>Tight(4%)</p>
+          <div className={styles.bittu1}>
+            <progress value="5" max="100"></progress>
+            <p>Short(5%)</p>
+          </div>
+          <div className={styles.bittu1}>
+            <progress value="4" max="100"></progress>
+            <p>ALittle Short(4%)</p>
+          </div>
+          <div className={styles.bittu1}>
+            <progress value="82" max="100"></progress>
+            <p>just Right(82%)</p>
+          </div>
+          <div className={styles.bittu1}>
+            <progress value="6" max="100"></progress>
+            <p>ALittle Loose(6%)</p>
+          </div>
+          <div className={styles.bittu1}>
+            <progress value="3" max="100"></progress>
+            <p>Loose(3%)</p>
           </div>
         </div>
       </div>
